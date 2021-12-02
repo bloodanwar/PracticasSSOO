c2c8062aa9ae8a376a156cd97f0ca8681af6d036
Carlos Rincón <38353262+bloodanwar@users.noreply.github.com>
2021-12-02 23:39:31 +0100

    IT's ALIVE

--- mifind.c
+++ mifind.c
@@ -5,55 +5,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdbool.h>    //libreria para declarar variables en bool
 
 #define PATH_MAX 4096    //Tamanio maximo del camino a un nodo
 
+int error(int problema){
+    switch(problema){
+        case 1:
+             printf("<<<ERROR>>> El comando -type esta mal escrito");
+            break;
+        case 2:
+            printf("<<<ERROR>>>error <-option> seguidos.");
+            break;
+        case 3:
+            printf("<<<ERROR>>> el comando -size está mal escrito");
+        break;
+    }
+    return problema;
+}
+
 //BUSQUEDA RECURSIVA
 int busqueda(const char *dir, int maxDepth, char type[], char size[], char name[], int currDepth){    //recibe parametros necesarios tales como maximaprofundidad, tipo, nombre, profundiad actual
     //DECLARACION DE VARIABLES
-
     DIR *d;
-    int encontrados=0;
+    int encontrados=0,tamanio;
     struct dirent *item;
     struct stat st;
     char nombre[PATH_MAX +1];
-    bool showReg=false, showDir=false, showSize=false, showName=false;
-  
-    //INSTRUCCIONES
     
-    //Type
-    if(strcmp(type,"")==0){ //estudiamos todos los posibles valores que puede obtener esta variable
-        showReg=true;
-        showDir=true;        
-    }
-    else if(strcmp(type,"d")==0){
-        showDir=true;
-    }else if(strcmp(type,"f")==0){
-        showReg=true;
-    }else{
-        printf("<<<ERROR>>> El comando -type esta mal escrito");    
-    }
-
     if((d=opendir(dir))==NULL){
         fprintf(stderr, "<<<ERROR>>> en la apertura del directorio %s\n ", dir);
-        return ;
+        exit(EXIT_FAILURE);
     }
     
     while ((item=readdir(d))!=NULL){
         if(strcmp(item->d_name,".")==0 || strcmp(item->d_name, "..")==0) continue;  //evitamos problemas con los directorios . y .. porque hacen bucles infinitos.
-        
-    
-        sprintf(nombre,"%s/%s",dir,item->d_name);
+            sprintf(nombre,"%s/%s",dir,item->d_name);
         if(stat(nombre,&st)==1){
             fprintf(stderr,"Error en la apertura de %s \n",item->d_name);
-            return;
+            exit(EXIT_FAILURE);
         }
         
-        //Size
-        showSize=true;
-    
-        //Nombre
+        bool showReg=false, showDir=false, showSize=false, showName=false;
+        //INSTRUCCIONES
+        //TYPE
+        if(strcmp(type,"")==0){ //estudiamos todos los posibles valores que puede obtener esta variable
+            showReg=true;
+            showDir=true;        
+        }
+        else if(strcmp(type,"d")==0){
+            showDir=true;
+        }else if(strcmp(type,"f")==0){
+            showReg=true;
+        }else{
+        error(1);   
+        }
+        //NAME
         if(strcmp(name,"")==0){ //estudiamos todos los posiles valores que puede obtener esta variable
             showName=true;
         }else if(strcmp(item->d_name,name)==0){ 
@@ -61,16 +69,46 @@ int busqueda(const char *dir, int maxDepth, char type[], char size[], char name[
         }else {
             showName=false;
         }
+
+        //SIZE 
+        if(strcmp(size,"")==0){
+            showSize=true;
+        }
+        else{    
+            char simbol;
+            int numero;
+            simbol=size[0];
+            numero=(atoi(&size[1]));
+
+            if (simbol=='-'){
+                if(st.st_size < numero){
+                    showSize=true;
+                }
+            }
+            else if (simbol=='+'){
+                if(st.st_size > numero){
+                    showSize=true;
+                }
+            }
+            else{
+                numero=(atoi(&size[0]));
+                if(st.st_size==numero){
+                    showSize=true;
+                }
+            }
+        }
         if(S_ISREG(st.st_mode)&&(showReg==true)&&(showSize==true)&&(showName==true)){   //si es un fichero regular a la vez que que el tamano, el nombre o la muestra de ficheros regulares sea verdadera entonces: 
             encontrados++;
-            printf("%s\n",item->d_name);   //mostrar contenido correspondiente->ficheros regulares
+            printf("%s con tamanio %ld\n",item->d_name,st.st_size);   //mostrar contenido correspondiente->ficheros regulares
         }   
         if(S_ISDIR(st.st_mode)){    //si es un directorio
             if((showDir==true)&&(showSize==true)&&(showName==true)){
                 encontrados++;
-                printf("%s\n",item->d_name);   //mostrar el contenido correspondiente-> directorios
+                showSize=false;
+                printf("%s con tamanio %ld\n",item->d_name,st.st_size);   //mostrar el contenido correspondiente-> directorios
             }
             if(maxDepth!=currDepth){    //si la profundidad maxima no es la misma que la profundidad actual
+                showSize=false;
                 encontrados+=busqueda(nombre,maxDepth, type, size, name, currDepth+1); //si es un directorio realizamos recursividad
             }    
         } 
@@ -78,11 +116,12 @@ int busqueda(const char *dir, int maxDepth, char type[], char size[], char name[
 
     closedir(d);
     return encontrados; //devuelve la cantidad de elementos encontrados
-
 }   //fin del metodo "busqueda"
+
 
 //FUNCION PRINCIPAL
 int main(int argc,char *argv[]){    //numero de argumentos y puntero con valor de argumentos 
+
 //DECLARACIÓN DE VARIABLES
 int DEPTH=0, ENCONTRADOS=0;
 bool errLin= false;
@@ -95,40 +134,35 @@ int checking=0;
 
 //MENU DE OPCIONES
 for(int i=1; i<argc; i++){  //recorre desde la primera posición hasta el número de argumentos indicado, éste aumenta
-
-if(strcmp(argv[i],"-maxdepth")==0){   //menú que depende de la posición de la línea de arguemtnos 
-    if(errLin==true){
-        printf("<<<ERROR>>>error <-option> seguidos.");
-    }
+    if(strcmp(argv[i],"-maxdepth")==0){   //menú que depende de la posición de la línea de arguemtnos 
+        if(errLin==true){
+            error(2);
+        }
     maxdpth=atoi (argv[i+1]);  //i es la opcion i+1 es el acompañante del argumento
     printf("maxdepth = %d\n",maxdpth);
     errLin=true;
-}else
-if(strcmp(argv[i],"-type")==0){ 
-    if(errLin==true){
-        printf("<<<ERROR>>>error <-option> seguidos.");
-    }
+    }else if(strcmp(argv[i],"-type")==0){ 
+        if(errLin==true){
+            error(2);
+        }
         sprintf(type,"%s",argv[i+1]);
         printf("Type = %s\n",type);
         errLin = true;
-}else
-if(strcmp(argv[i],"-name")==0){ 
+    }else if(strcmp(argv[i],"-name")==0){ 
     if(errLin==true){
-        printf("<<<ERROR>>>error <-option> seguidos.");
+        error(2);
     }
         sprintf(name,"%s",argv[i+1]);
         printf("name = %s\n",name); 
         errLin = true;   
-}else
-if(strcmp(argv[i],"-size")==0){ 
-    if(errLin==true){
-        printf("<<<ERROR>>>error <-option> seguidos.");
-    }
+    }else if(strcmp(argv[i],"-size")==0){ 
+        if(errLin==true){
+            error(2);
+        }
         sprintf(size,"%s",argv[i+1]);
         printf("Size = %s\n",size);
         errLin=true;
-}else
-    if (errLin==false){
+    }else if (errLin==false){
         if(strcmp(argv[1],argv[i])==0){
             sprintf(ruta,"%s",argv[i]);
         }else{
@@ -142,10 +176,7 @@ if(strcmp(argv[i],"-size")==0){
 int encontrados=0;
 
 printf("------------------\n");
-
 encontrados=busqueda(ruta,maxdpth,type,size,name,0);
-
 printf("\nSe han encontrado %d archivos\n",encontrados);
-
 return 0;
 }
